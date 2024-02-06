2cbc7a0880ded660cb58575971b88518fdac1305
Baugette9000 <evanroe.icloud@gmail.com>
2024-02-05 21:18:42 -0800

    configured xrandr and moved .nix files to pkgs

--- pkgs/nvidia/nvidia.nix
+++ pkgs/nvidia/nvidia.nix
@@ -19,6 +19,24 @@
     open = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable; 
+    };
 
-  };
+    services.xserver = {
+     config = ''
+      Section "Device"
+      Identifier "nvidia"
+      Driver "nvidia"
+      BusID "PCI:0b:00:0"
+      Option "AllowEmptyInitialConfiguration"
+      EndSection
+    '';
+     screenSection = ''
+      Option         "metamodes" "nvidia-auto-select +0+0 {ForceFullCompositionPipeline=On}"
+      Option         "AllowIndirectGLXProtocol" "off"
+      Option         "TripleBuffer" "on"
+    '';
+    displayManager.setupCommands = ''
+    ${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-0 --auto --output DP-1 --right-of HDMI-0
+    '';
+    };
 }
