66cd4229b6bcacc6b78178de896c0544861317ce
Baugette9000 <evanroe.icloud@gmail.com>
2024-02-05 23:05:51 -0800

    enabled kernel_latest due to patch being released

--- configuration.nix
+++ configuration.nix
@@ -19,7 +19,7 @@
     ];
 
   # Bootloader.
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_5;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
