54f1066d6eeb054d5abc5db30932cf29eed8bb57
Baugette9000 <evanroe.icloud@gmail.com>
2024-02-07 19:23:36 -0800

    Revert "fixed dwm"
    This reverts commit d85e68e37a980ccc84d125536526f5c54ecde607.

--- pkgs/dwm/dwm.nix
+++ pkgs/dwm/dwm.nix
@@ -2,13 +2,9 @@
 
 {
   services.xserver.windowManager.dwm.enable = true;
-    services.xserver.windowManager.dwm.package = pkgs.dwm.override {
-    conf = /home/cleo/.dotfilesNix/suckless/dwm;
-    patches = [ /home/cleo/.dotfilesNix/suckless/dwm ]; # Or some `fetchPatch` thing
-  };
-  #nixpkgs.overlays = [
-  #   (final: prev: {
-	#dwm = prev.dwm.overrideAttrs (old :{ src = /home/cleo/.dotfilesNix/suckless/dwm;});
-  #    })
-  #];
+    nixpkgs.overlays = [
+     (final: prev: {
+	dwm = prev.dwm.overrideAttrs (old :{ src = /home/cleo/.dotfilesNix/suckless/dwm ;         }); 
+      })
+   ];
 }
